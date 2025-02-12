956fbed9781bdc3b2a7f99c0cd880ff89e44b063
stillgre <ivan.grechishkin@bk.ru>
2025-02-12 16:14:32 +0300

    teacher_income.sql

--- teacher_income.sql
+++ teacher_income.sql
@@ -1,11 +1,11 @@
-select 
+SELECT
 full_name,
 teacher_id,
-case
-    when datediff('2025-01-01', start_dt) < 365*3 then 1000
-    when datediff('2025-01-01', start_dt) between 3*365 and 6*365 then 3000
-    when datediff('2025-01-01', start_dt) > 6*365 then 10000
-end as income
-from teachers
-where end_dt is null
-order by income desc;
+CASE
+    WHEN datediff('2025-01-01', start_dt) < 365*3 THEN 1000
+    WHEN datediff('2025-01-01', start_dt) BETWEEN 3*365 AND 6*365 THEN 3000
+    WHEN datediff('2025-01-01', start_dt) > 6*365 THEN 10000
+END AS income
+FROM teachers
+WHERE end_dt IS NULL
+ORDER BY income DESC;
